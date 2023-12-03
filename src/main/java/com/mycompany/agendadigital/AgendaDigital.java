e2b040403d460c206ac947be94756eaad5f36422
VictorSalasProgram <victorsalasok@gmail.com>
2023-12-03 12:43:23 -0300

    Proyecto
    proyecto de agenda

---
+++ src/main/java/com/mycompany/agendadigital/AgendaDigital.java
@@ -0,0 +1,13 @@
+package com.mycompany.agendadigital;
+
+import com.mycompany.agendadigital.igu.Principal;
+
+
+public class AgendaDigital {
+
+    public static void main(String[] args) {
+        Principal screen = new Principal();
+        screen.setVisible(true);
+        screen.setLocationRelativeTo(null);
+    }
+}
